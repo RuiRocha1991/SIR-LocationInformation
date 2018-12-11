6a72f7a9659d17ecaf4a2d4df0ca0a609e01f23d
Rui Rocha <ruirocha1991@gmail.com>
2018-12-11 18:21:03 +0000

    finished index restruct the css

--- php/placesProxy.php
+++ php/placesProxy.php
@@ -5,7 +5,7 @@
 		$type=$_GET['type'];
 		$radius=$_GET['radius'];
 		$BASE_URL="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=";
-		$API_KEY ="&key=AIzaSyAPwAin8WQ_Ous1cp9MLAKZW-SAmYHsPpQ";
+		$API_KEY ="&key=AIzaSyD3_KAi8uClL6LVrkN7K0PSRxJsWuvplkY";
 		$url=$BASE_URL.$latitude.','.$longitude."&radius=".$radius."&type=".$type.$API_KEY;
 		echo file_get_contents($url);
 	}
