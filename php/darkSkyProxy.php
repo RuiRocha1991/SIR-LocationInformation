4f0694c6d320dae1fae164f624bddf2212a871a9
RuiRocha1991 <ruirocha1991@gmail.com>
2019-06-04 17:56:22 +0100

    Update darkSkyProxy.php

--- php/darkSkyProxy.php
+++ php/darkSkyProxy.php
@@ -3,7 +3,7 @@
 		$latitude=$_GET['lat'];
 		$longitude=$_GET['lng'];
 		$DARKSKY_BASE_URL="https://api.darksky.net/forecast/";
-		$DARKSKY_API_KEY ="a0b9deac815015674faa8c422837e39b";
+		$DARKSKY_API_KEY ="";
 		$darksky_url=$DARKSKY_BASE_URL.$DARKSKY_API_KEY.'/'.$latitude.','.$longitude."?units=si";
 		echo file_get_contents($darksky_url);
 	}
